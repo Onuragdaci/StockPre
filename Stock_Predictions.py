d21f0acb96f854ebb818a9f3002a43d9953895cd
Onur AĞDACI <123022449+Onuragdaci@users.noreply.github.com>
2023-01-24 00:30:37 +0300

    Update Stock_Predictions.py

--- Stock_Predictions.py
+++ Stock_Predictions.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 from pandas.tseries.offsets import CustomBusinessHour , BDay
 from prophet import Prophet
-from prophet.plot import plot_plotly, plot_components_plotly ,plot_forecast_component_plotly 
+from prophet.plot import plot_plotly, plot_components_plotly ,plot_seasonality_plotly
 import ssl
 from urllib import request
 #python -m streamlit run app.py
@@ -24,14 +24,14 @@ st.set_page_config(
     layout="wide",                                                              #Sayfa Formatı
     initial_sidebar_state="expanded")
 
-lstAll=['1-Minute','3-Minute','5-Minute','15-Minute','30-Minute','45-Minute','1-Hour','2-Hour','3-Hour','4-Hour','1 Day']      #Görsel Zaman Aralığı
-lst1=['1','3','5','15','30','45','1H','2H','3H','4H','1D']                                                             #Alt Zaman Aralığı
-lst2=['1T','3T','5T','15T','30T','45T','1H','2H','3H','4H','D']                                                        #Alt Zaman Aralığı
+lstAll=['1-Minute','3-Minute','5-Minute','15-Minute','30-Minute','45-Minute','1-Hour','2-Hour','3-Hour','4-Hour']      #Görsel Zaman Aralığı
+lst1=['1','3','5','15','30','45','1H','2H','3H','4H']                                                             #Alt Zaman Aralığı
+lst2=['1T','3T','5T','15T','30T','45T','1H','2H','3H','4H',]                                                        #Alt Zaman Aralığı
 with st.sidebar:
     Hisse_Temel=Hisse_Temel_Veriler()                                                                                  #Hisse Başlıkları
     st.header('Hisse Arama')                                                                                           #Yan Pencere Adı
     dropdown1 = st.selectbox('Hisse Adı',Hisse_Temel['Kod'])                                                           #Hisse Seçiminin Belirtilmesi
-    dropdown2 = st.selectbox('Zaman Aralığı',lstAll,index=10)                                                          #Hisse Zaman Aralığının Belirtilmesi - Varsayılan 1 Saat    
+    dropdown2 = st.selectbox('Zaman Aralığı',lstAll,index=9)                                                           #Hisse Zaman Aralığının Belirtilmesi - Varsayılan 1 Saat    
     dropdown3 = st.slider('Data Sayısı', 0, 10000, 1000, 100)                                                          #İncelenecek Data Sayısının Belirtilmesi - Varsayılan 5000 Birim
     dropdown4 = st.slider('Gün Tahmini', 0, 20, 5, 5)                                                                  #Tahmin Edilecek Gün Sayısının Belirtilmesi - Varsayılan 5 Gün
 
@@ -40,14 +40,13 @@ index=lstAll.index(dropdown2)                                                   
 data = tv.get_hist(symbol=dropdown1,exchange='BIST',interval=Interval(lst1[index]),n_bars=dropdown3)                   #TradingView dan Hisseye Sit Datanın İlgili Zaman Aralığında Çekilmesi
 df_train=pd.DataFrame({"ds":data.index.values,"y":data['close'].values})                                               #Eğitim Kümesinin Oluşturulması
 
-st.header(dropdown1+' Analizi')                                                                                        #Hisseye Ait Ana Sayfa Başlığının Oluşturulması
+st.header(dropdown1+' Analizi')                                                                                         #Hisseye Ait Ana Sayfa Başlığının Oluşturulması
 fig1= go.Figure()                                                                                                      #Plotly 1 Görselinin Oluşturulması
 fig1.add_trace(go.Scatter(x=data.index,y=data["close"],name="Hisse Fiyatı"))                                           #Plotly 1 Hisse Fiyat Görselinin Oluşturulması
 st.plotly_chart(fig1)                                                                                                  #Plotly 1 Görselinin Sayfaya Yüklenmesi
 
-m=Prophet()                                                                                                            #Makine Öğrenme Algoritmasının Çağırılması
+m=Prophet(daily_seasonality=True)                                                                                      #Makine Öğrenme Algoritmasının Çağırılması
 m.fit(df_train)                                                                                                        #Eğitim Kümesinin Fit Edilmesi
-
 cbh = pd.tseries.offsets.CustomBusinessHour(n = 1, weekmask = 'Mon Tue Wed Thu Fri', start ='10:00', end="18:00")      #BIST Çalışma Günlerinin ve Saatlerinin Belirlenmesi
 
 startdate=data.index[-1]                                                                                               #Tahminin Yapılacağı Başlangıç Tarihinin Belirlenmesi
@@ -58,17 +57,24 @@ future2=pd.bdate_range(start=startdate,end=enddate,freq=lst2[index])            
 future2=pd.DataFrame({"ds":future2})                                                                                   #Gelecek Tahmin Kümesinin OLuşturulması
 future2=future2[pd.to_datetime(future2["ds"]).map(cbh.is_on_offset)]                                                   #Gelecek Tahmin Kümesinin Çalışma Saatlerine Göre Filtrelenmesi
 
-if index==10:
-    startdate=data.index[-1]                                                                                           #Tahminin Yapılacağı Başlangıç Tarihinin Belirlenmesi
-    enddate=(startdate+BDay(n=dropdown4))                                                                              #Tahminin Yapılacağı Son Gün ve Kapanış Saatinin Belirlenmesi
-    future2=pd.bdate_range(start=data.index[-1],end=enddate,freq='B').strftime("%Y-%m-%d 10:00:00")
-    future2=pd.DataFrame({"ds":future2})
 futureAll=pd.concat([future1,future2],axis=0,ignore_index=True)                                                        #Geçmiş ve Gelecek Tahmin Kümelerinin Birleştirilmesi
 forecast=m.predict(futureAll)                                                                                          #Tahminin Yapılması
 st.write("Tahmin Grafikleri")                                                                                          #Sayfaya Tahmin Grafikleri Başlığının Yazılması
 fig2=plot_plotly(m,forecast)                                                                                           #Plotly 2 Görselinin Oluşturulması
 st.plotly_chart(fig2)                                                                                                  #Sayfaya Tahmin Grafiklerinin Yüklenmesi
 
-st.write("Tahmin Bileşenleri")                                                                                         #Sayfaya Tahmin Bileşenleri Başlığının Yazılması
-fig3=plot_components_plotly(m, forecast,figsize=(1600, 300))                                                           #Plotly 3 Görselinin Oluşturulması
-st.write(fig3)                                                                                                         #Sayfaya Tahmin Bileşenlerinin Yüklenmesi                                 
+st.write('Günlük Tahmini Değişim')
+fig3 = plot_seasonality_plotly(m, name='daily')
+MAX_Daily=forecast['daily'].max()
+MIN_Daily=forecast['daily'].min()
+fig3.update_xaxes(rangebreaks=[dict(bounds=[18, 10], pattern="hour")])
+fig3.update_yaxes(range=(MIN_Daily-0.1,MAX_Daily+0.1))
+st.write(fig3)
+
+st.write('Haftalık Tahmini Değişim')
+fig4 = plot_seasonality_plotly(m, name='weekly')
+MAX_Weekly=forecast['weekly'].max()
+MIN_Weekly=forecast['weekly'].min()
+fig4.update_xaxes(rangebreaks=[dict(bounds=["sat", "mon"],pattern="day of week")])
+fig4.update_yaxes(range=(MIN_Weekly-0.1,MAX_Weekly+0.1))
+st.write(fig4)
