a4d8ef5b06174188fed4b39750a75fd070a011c1
Onur AĞDACI <123022449+Onuragdaci@users.noreply.github.com>
2023-08-10 14:57:50 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -31,8 +31,20 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
-hide_icon="""#MainMenu {visibility: hidden;}"""
-st.markdown(hide_icon, unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 with st.sidebar:
     Hisse_Ozet=Hisse_Temel_Veriler()
     st.header('Hisse Arama')
