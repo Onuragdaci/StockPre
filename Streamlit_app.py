20d8b2483ce391f7b5e2d75a0ebae2706942ed9a
Onur AĞDACI <123022449+Onuragdaci@users.noreply.github.com>
2023-08-22 19:55:29 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -316,7 +316,8 @@ Tum_Carpanlar_2['İçsel Değer'] = Tum_Carpanlar_2['İçsel Değer'].apply(lamb
 Tum_Carpanlar_2['Marj']=((Tum_Carpanlar_2['İçsel Değer']-Tum_Carpanlar_2['Fiyat'])/(Tum_Carpanlar_2['Fiyat']+0.001))*100
 Tum_Carpanlar_2['Marj'] = Tum_Carpanlar_2['Marj'].apply(lambda x: round(x, 2))
 Tum_Carpanlar_2=Tum_Carpanlar_2.T
-with col1:
-    st.dataframe(Tum_Carpanlar,use_container_width=True)
-with col2:
-    st.dataframe(Tum_Carpanlar_2,use_container_width=True)
+col1.subheader('Yıllıklandırılmış Verilere Göre')
+col1.dataframe(Tum_Carpanlar,use_container_width=True,height=2000 )
+
+col2.subheader('Tahmii Yıl Sonu Verilerine Göre')
+col2.dataframe(Tum_Carpanlar_2,use_container_width=True,height=2000 )
