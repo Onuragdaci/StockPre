c638ca1320ed3f344008fb68c9d2e7888e166100
Onur AĞDACI <123022449+Onuragdaci@users.noreply.github.com>
2023-08-22 19:02:27 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -48,7 +48,7 @@ with st.sidebar:
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version='116.0.5845.96').install()), options=options)
   
 def Yıllıklandirilmiş_Veriler(Hisse):
     print(Hisse)
