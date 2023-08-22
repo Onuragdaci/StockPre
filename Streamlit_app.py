1b8bb1b9dd0ea215b08c578462c0bf549029b354
Onur AĞDACI <123022449+Onuragdaci@users.noreply.github.com>
2023-08-22 19:07:26 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -179,7 +179,7 @@ for i in range(len(Hisse_Adı)):
     Tah_Yıllık_Kar=Tah_NetKar
     
     Yıllık_Satıs=LL05
-    Tah_Yıllık_Satıs=Tah_NetSat
+    Tah_Yıllık_Sat=Tah_NetSat
 
     Gelecek_FK=PiyDeg/(Yıllık_Kar+0.0001)
     Gelecek_FK_2=PiyDeg/(Tah_Yıllık_Kar+0.0001)
@@ -191,7 +191,7 @@ for i in range(len(Hisse_Adı)):
     Potansiyel_PD_2=Tah_Yıllık_Kar*7+ÖzSer*0.5
     
     NetKarMarjı=Yıllık_Kar/(Yıllık_Satıs+0.0001)                   #Net Kâr Marjı
-    NetKarNarjı_2=Tah_Yıllık_Kar/(Tah_Yıllık_Satıs+0.0001)
+    NetKarNarjı_2=Tah_Yıllık_Kar/(Tah_Yıllık_Sat+0.0001)
     PD_NS=PiyDeg/(Yıllık_Satıs +0.0001)                            #Piyasa Değeri / Net Satışlar
     PD_NS_2=PiyDeg/(Tah_Yıllık_Sat+0.0001)
 
