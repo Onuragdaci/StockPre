782b210c75cf0d9fcba1236229a9683c1e5e9f0d
Onur AĞDACI <123022449+Onuragdaci@users.noreply.github.com>
2023-09-10 19:01:12 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import pandas as pd
 import numpy as np
